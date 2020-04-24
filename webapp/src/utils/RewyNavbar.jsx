53bdcd46f1dfbb2405765f655edef8b1e6ffb829
Kenny Hoang <info@kennyhoang.se>
2020-04-24 13:59:37 +0200

    added basic styling on navbar and also fixed homepage with navigation problems

--- webapp/src/utils/RewyNavbar.jsx
+++ webapp/src/utils/RewyNavbar.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
-import {Navbar,Nav,Form,Button,NavDropdown} from 'react-bootstrap';
+import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useSelector, useDispatch } from 'react-redux';
+import {signOut} from '../actions/login';
+import './RewyNavbar.css'
 
 
 function RewyNavbar() {
 
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state=>state.loginReducer.isLoggedIn);
 
     return (
 
 <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
   <Navbar.Brand href="/">Rewy</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/login">Login</Nav.Link>
-      <Nav.Link href="/#">Register</Nav.Link>
-    </Nav>
+  <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
     <FontAwesomeIcon icon={faSearch} />
-<NavDropdown title="dropdown" id="collasible-nav-dropdown">
+    {!isLoggedIn ? 
+      <Nav >
+      <Nav.Link href="/register">Register</Nav.Link>
+      <Nav.Link href="/login">Login</Nav.Link>
+    </Nav>:
+    <NavDropdown title={ <FontAwesomeIcon className="dropdown-icon" icon={faUser} />} id="collasible-nav-dropdown">
         <NavDropdown.Item href="/">profile</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="/">Sign out</NavDropdown.Item>
-      </NavDropdown>
-
+        <NavDropdown.Item onClick={() => dispatch(signOut())}>Sign out</NavDropdown.Item>
+      </NavDropdown>}
   </Navbar.Collapse>
 </Navbar>
     ); 

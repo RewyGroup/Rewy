ea9c34ec67c1d614a2f5b44f38c6660fcd052809
Kenny Hoang <info@kennyhoang.se>
2020-05-08 14:57:59 +0200

    added a card for profileCard and installed react-avatar

--- webapp/src/components/Profile/ProfileCard.jsx
+++ webapp/src/components/Profile/ProfileCard.jsx
@@ -1,16 +1,53 @@
-import React from "react";
-import { Row, Col, Card, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { Row, Col, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import Avatar from 'react-avatar-edit';
 import "./ProfileCard.css";
 
-const ProfileCard = () => {
+const ProfileCard = (props) => {
+
+  const {user} = props;
+  const dispatch = useDispatch();
+
+  const [preview,setPreview] = useState(null);
+  const [imageSrc,setImageSrc] = useState("/logo.jpg");
+  const [changeProfileImage,setChangeProfileImage] = useState(false);
+
+  
+  console.log(user);
+  
+
+  function onClose() {
+  }
+  
+  function onCrop(preview) {
+    setPreview({preview});
+  }
+
+  function onBeforeFileLoad(elem) {
+    if(elem.target.files[0].size > 71680){
+      alert("File is too big!");
+      elem.target.value = "";
+    };
+  }
+
+  const profileImgClick =(event) =>{
+    console.log("hello");
+    setChangeProfileImage(true);
+    
+  }
+
+
   return (
     <Row className="profileCardRow">
       <Col xs={6} md={12}>
-        <Card className="profileCard">
-          <Card.Img src="/banner.jpg" alt="Card image" />
-          <Card.ImgOverlay>
+        <div className="profileCardWrapper">
+        <Card className="profileCardBanner">
+          <Card.Img className="profileCardImg" src="/banner.jpg" alt="Card image" />
+          <Card.ImgOverlay className="profileImgOverlay">
             <div className="profileCardEditButtonWrapper">
               <FontAwesomeIcon
                 className="profileCardEditButton"
@@ -18,14 +55,28 @@ const ProfileCard = () => {
               />
             </div>
           </Card.ImgOverlay>
+
         </Card>
-        <Card>
+
+        <Card className="profileCardInfo">
+        {preview ? 
+           <img className="profileImage" src={preview.preview} alt="Preview"/>
+         :<img className="profileImage" src={imageSrc} alt="Preview" />}
           <Card.Body>
-            <div className="profileCardButtonWrapper" >
-            <Button className="ProfileCardButton">press me</Button>
+            <div className="profileInfoHeader">
+            <Card.Title>firstname</Card.Title>
             </div>
+            <Card.Text>{user.firstName}</Card.Text>
+            <Card.Title>lastname</Card.Title>
+            <Card.Text>{user.lastName}</Card.Text>
+            <Card.Title>mail</Card.Title>
+            <Card.Text>{user.email}</Card.Text>
+            <Avatar width={200} height={200} 
+         onCrop={onCrop} onClose={onClose}
+         onBeforeFileLoad={onBeforeFileLoad} src={imageSrc}/>
           </Card.Body>
         </Card>
+         </div>
       </Col>
     </Row>
   );
--- webapp/src/pages/ProfilePage.jsx
+++ webapp/src/pages/ProfilePage.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import {stillLoggedIn} from '../actions/login';
 import {Cookies} from 'react-cookie';
 import {Container} from 'react-bootstrap';
+import {getUserById} from '../actions/user';
 import ProfileCard from '../components/Profile/ProfileCard';
 
 function ProfilePage(props) {
@@ -11,8 +12,17 @@ function ProfilePage(props) {
 
     const cookies = new Cookies();
     const session_token = cookies.get("session_token");
-
-
+    const userId = useSelector(state => state.loginReducer.user.id);
+    const optionalUser = useSelector(state => state.userReducer.user);
+    
+    const user = optionalUser && optionalUser.id && <ProfileCard user={optionalUser}/>;
+    
+    
+    useEffect(()=>{
+        dispatch(getUserById(userId));
+    
+        },[]);
+    
     if(session_token){
         dispatch(stillLoggedIn(session_token));
     }
@@ -20,7 +30,7 @@ function ProfilePage(props) {
         return (
             <div>
                 <Container>
-                <ProfileCard/>
+                {user}
                 </Container>
             </div>
         );

95a986554438db8e4fe52a3076b9ec82209b817c
Kenny Hoang <info@kennyhoang.se>
2020-05-20 12:03:58 +0200

    Api is not called if not during reload for questions

--- webapp/src/pages/QuestionPage.jsx
+++ webapp/src/pages/QuestionPage.jsx
@@ -7,14 +7,21 @@ import { getQuestionById } from '../actions/question';
 import Question from '../components/Question/Question'
 
 const QuestionPage = (props) => {
+
+  const {question} = props.history.location;
+
+  
+  
     const dispatch = useDispatch();
     var parts = props.location.pathname.split('/');
     const id = parts[parts.length - 1];   
     const [checker,setChecker] = useState(true);
+    const [activeQuestion,setActiveQuestion] = useState(null);
     const cookies = new Cookies();
     const session_token = cookies.get("session_token");
-    const question = useSelector(state => state.questionReducer.question); 
+    const newQuestion = useSelector(state => state.questionReducer.question); 
     const isLoggedIn = useSelector((state) => state.loginReducer.isLoggedIn);
+    const questionLoaded = useSelector((state) => state.questionReducer.questionLoaded);
 
     if(checker){
         if (session_token) {
@@ -25,10 +32,23 @@ const QuestionPage = (props) => {
 
 
     useEffect(() => {
-            dispatch(getQuestionById(id,session_token));    
+        if(questionLoaded){  
+          setActiveQuestion(question); 
+        }else{
+          dispatch(getQuestionById(id,session_token));  
+        }
         }, [])
 
-        const selectedQuestion = question &&  question.category && <Question history={props.history} isLoggedIn={isLoggedIn} token={session_token} question={question}></Question>
+        useEffect(() => {
+
+          if(activeQuestion === null && newQuestion.category){
+              
+            setActiveQuestion(newQuestion);
+            }
+          }, [newQuestion])
+          
+
+        const selectedQuestion = activeQuestion &&  activeQuestion.category && <Question history={props.history} isLoggedIn={isLoggedIn} token={session_token} question={activeQuestion}></Question>
         
 
         return (<Container>{selectedQuestion} </Container> );
